1a0a3a53365fb0c153082c1a651656eedb1d6fe8
HieuNMFrovis <hieunm.frovis@gmail.com>
2024-05-07 11:36:14 +0700

    update ui gridview and not completed yet

--- app/src/main/java/com/example/listnh/MainActivity.kt
+++ app/src/main/java/com/example/listnh/MainActivity.kt
@@ -1,21 +1,46 @@
 package com.example.listnh
 
+import android.graphics.ColorSpace
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.listnh.adapter.MainAdapter
 import com.example.listnh.databinding.ActivityMainBinding
+import com.example.listnh.model.ImageModel
 import com.example.listnh.utils.SampleData
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    var ImageModel = ArrayList<ColorSpace.Model>()
+    var name = arrayOf(
+        "image2",
+        "image2",
+        "image2",
+        "image2",
+        "image2",
+        "image2",
+        "image2",
+        "image2",
+        "image2",
+        "image2"
+    )
+    var images = intArrayOf(
+        R.drawable.image2,
+        R.drawable.image2,
+        R.drawable.image2,
+        R.drawable.image2,
+        R.drawable.image2,
+        R.drawable.image2,
+        R.drawable.image2,
+        R.drawable.image2,
+        R.drawable.image2,
+        R.drawable.image2,)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.apply {
-            rvimage.adapter = MainAdapter(SampleData.collection)
-        }
     }
+    class CustomAdapter(
+        var itemModel: ArrayList <ImageModel>,
+    )
 }
