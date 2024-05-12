95f807a1f52a564a88cbcbfcbf9c066a34170832
HieuNMFrovis <128480205+HieuNMFrovis@users.noreply.github.com>
2024-05-12 23:16:43 +0700

    update ui gridview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("io.coil-kt:coil:2.1.0")
-    implementation ("com.github.ismaeldivita:chip-navigation-bar:1.4.0")
+//    implementation ("io.coil-kt:coil:2.1.0")
+//    implementation ("com.github.ismaeldivita:chip-navigation-bar:1.4.0")
 }
